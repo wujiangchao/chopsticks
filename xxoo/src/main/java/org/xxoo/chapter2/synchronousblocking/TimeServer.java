d5191167e789e32af34b657f1eac5301311a4439
wujiangchao <342464096@qq.com>
2018-06-08 22:09:56 +0800

    hhh

---
+++ xxoo/src/main/java/org/xxoo/chapter2/synchronousblocking/TimeServer.java
@@ -0,0 +1,11 @@
+package org.xxoo.chapter2.synchronousblocking;
+/**
+ * 传统的同步阻塞式I/O
+ * @author jack
+ *
+ */
+public class TimeServer {
+    public static void main(String[] args) {
+        
+    }
+}
